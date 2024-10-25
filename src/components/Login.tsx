ed144d70f85ba86190a4d2e42be1a204e183ba86
sannariipinen <sanna.riipinen@student.laurea.fi>
2024-10-25 13:29:30 +0300

    yritys

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from 'react';
-import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { Form, Button, Card } from 'react-bootstrap';
 import { Link, useHistory } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
